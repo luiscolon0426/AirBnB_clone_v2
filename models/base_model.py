73584a9b83d9d2555d18087bb4bd2521f7ac9150
Soly-M-27 <solymar227@icloud.com>
2022-04-10 00:10:56 +0000

    Adding new features to start mapping with sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,10 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+Base = declarative_base()
 
 
 class BaseModel:
@@ -10,10 +14,9 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.id = Column(String(60), unique=True, primary_key=True, nullable=False)
+            self.created_at = Column(datetime.utcnow(), nullable=False)
+            self.updated_at = Column(datetime.utcnow(), nullable=False)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -21,6 +24,9 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
+            for x, y in kwargs.items():
+                setattr(args[1], x, y)
+            ''' storage.save()??? '''
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -31,6 +37,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
