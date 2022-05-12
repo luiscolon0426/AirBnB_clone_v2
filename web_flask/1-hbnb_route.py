5915afd44baec3a3800c1cf9bef29bce8624b780
Luis Colon <luiscolon0426@gmail.com>
2022-05-12 16:39:14 -0400

    Added

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' Script that starts a Flask Web Aplication '''
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def rerout():
+    return("HBNB")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001, debug=False)
